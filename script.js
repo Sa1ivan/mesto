2db63cf7888b74b24c8d7741d4b1499f2386e372
Khadzhiev Deni <deni555mech@mail.ru>
2021-11-26 23:22:57 +0300

    mesto final

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
 const popupOpenBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
-const popupUsername = document.querySelector('.profile__username');
-const popupUserInfo = document.querySelector('.profile__user-info');
-const popupNewText = document.querySelectorAll('.popup__text');
-const checkedClickBtn = 1;
+let userName = document.querySelector('.profile__username');
+let popupName = document.querySelector('.popup__username-text'); 
 const submitBtn = document.querySelector('.popup__submit-btn');
+let userInfo = document.querySelector('.profile__user-info');
+let popupInfo = document.querySelector('.popup__text');
+const overlay = document.querySelector('.popup__overlay');
 
-
-
+popupName.value= 'Жак-Ив Кусто';
+popupInfo.value = 'Исследователь океана';
 
 
 function openPopup (){
     popup.classList.add('popup_active');
+    popupName.value;
+    popupInfo.value;
 }
 
 function closePopup (){
     popup.classList.remove('popup_active');
-    checkedClickBtn = 0;
+}
+
+
+function formSubmitHandler(){
+    userName.textContent = popupName.value;
+    userInfo.textContent = popupInfo.value;
 }
 popupOpenBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
-
-function textRename(){
-    if (checkedClickBtn === 1){
-        popupUsername.textContent.remove('profile__username');
-        popupUsername.textContent.add('popapText');
-    }
-    else{
-        popupCloseBtn.addEventListener('click', closePopup);
-    }
-}
-popupUsername.addEventListener('click', textRename);
-submitBtn.addEventListener('click', submit);
-function submit(){
-    popupUsername.textContent.add('popapText');
-}
+submitBtn.addEventListener('click', formSubmitHandler);
+submitBtn.addEventListener('click', closePopup);
+overlay.addEventListener('click', closePopup);
